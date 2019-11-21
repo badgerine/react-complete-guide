8597f76f1b02db9e274c974360d5b2d9d0d77881
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-11-21 11:36:43 +0200

    S5#73 working with css modules. Removing Styled Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 // import React, {useState} from 'react';
-import styled from 'styled-components';
 import './App.css';
 import Person from './Person/Person';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px,
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black
-  }
-`;
 
 class App extends Component {
 
@@ -89,9 +74,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app</h1>
         <p className={styleclass.join(' ')}>I'm growing</p>
-        <StyledButton
-          alt={this.state.showPersons}
-          onClick={this.togglePersonsHandler}>Show People</StyledButton>
+        <button className="button" onClick={this.togglePersonsHandler}>
+            Show People
+        </button>
         <p />
         {persons}
       </div>
