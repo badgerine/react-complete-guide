6564ec68977eebd41fb4205bcad04066c547d7dd
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-11-22 15:25:31 +0200

    S7#85 removing ErrorBoundary

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -3,11 +3,6 @@ import classes from './Person.css';
 
 const person = (props) => {
     
-    const rnd = Math.random();
-    if(rnd > 0.7){
-        throw new Error('oopsi, woopsi');
-    }
-        
     return (
         <div className={classes.Person}>
             < input type="text" onChange={props.liveMod} value={props.name} />
