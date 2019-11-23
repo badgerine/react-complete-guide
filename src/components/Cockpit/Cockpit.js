ae7a4269414f0ed69b7abbf4fb4890a202026b2e
sibusiso matsini <sbu.matsini@gmail.com>
2019-11-23 23:24:36 +0200

    S7#94 controlling the useEffect() behaviour | functional components cannot use component lifecycle hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
 
-    let btnClass = [classes.Button];
-    const styleclass = [];
-    if (props.personCollection.length <= 2) {
-      styleclass.push(classes.red);
-    }
-    if (props.personCollection.length <= 1) {
-      styleclass.push(classes.bold);
-    }
+  useEffect(() =>{
+    console.log('[Cockpit.js] useEffect()');
 
-    if(props.showPersons){
-        btnClass.push(classes.Red);
-    }
+    //Http request ...
+    setTimeout(() => {
+      alert('saved data to cloud!');
+    }, 1000);
+  }, []); //this empty array indicates to useEffect to only execute the 1st time this component is rendered.
 
-    return (
-        <div>
-            <h1>{props.title}</h1>
-            <p className={styleclass.join(' ')}>I'm growing</p>
-            <button className={btnClass.join(' ')} onClick={props.clicked}>
-                Show People
+  let btnClass = [classes.Button];
+  const styleclass = [];
+  if (props.personCollection.length <= 2) {
+    styleclass.push(classes.red);
+  }
+  if (props.personCollection.length <= 1) {
+    styleclass.push(classes.bold);
+  }
+
+  if (props.showPersons) {
+    btnClass.push(classes.Red);
+  }
+
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p className={styleclass.join(' ')}>I'm growing</p>
+      <button className={btnClass.join(' ')} onClick={props.clicked}>
+        Show People
         </button>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default cockpit;
