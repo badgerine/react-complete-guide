8cb16f1d0afeb10cff08e4e9efe5cd5f17842261
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-11-25 15:07:35 +0200

    S7#105 higher order components withprops

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import classes from './Person.css';
+import personClasses from './Person.css';
+import WithClass from '../hoc/WithClass';
 
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <WithClass classes={[personClasses.Person]}>
                 < input type="text" onChange={this.props.liveMod} value={this.props.name} />
                 <p onClick={this.props.click}>i'm {this.props.name} and i am {this.props.age} years old! {this.props.children}</p>
-            </div>
+            </WithClass>
 
         )
     };
