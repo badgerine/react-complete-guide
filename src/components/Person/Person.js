baef6c67934a50c414710b60072529e0a5466ce3
Sibusiso Matsini <Sibusiso.Matsini@altron.com>
2019-11-25 17:05:13 +0200

    S7#110 using refs

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -4,11 +4,27 @@ import WithClass from '../hoc/WithClass';
 import PropTypes from 'prop-types';
 
 class Person extends Component {
+
+    constructor(props){//always add props to ensure app works as expected!!
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         return (
             <WithClass classes={[personClasses.Person]}>
-                < input type="text" onChange={this.props.liveMod} value={this.props.name} />
+                < input 
+                type="text" 
+                // ref={(inputEl) => {this.inputElement = inputEl;}}//only works in class based components!!
+                ref = {this.inputElementRef}
+                onChange={this.props.liveMod} 
+                value={this.props.name} />
                 <p onClick={this.props.click}>i'm {this.props.name} and i am {this.props.age} years old! {this.props.children}</p>
             </WithClass>
 
